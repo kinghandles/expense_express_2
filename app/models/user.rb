3ac10f3c1d695faa7e729a1ec8428280f66fc2df
Raghu Betina <raghu@firstdraft.com>
2019-12-05 03:08:16 +0000

    Direct association between users and individual_expense_ledgers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :individual_expense_ledgers,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
