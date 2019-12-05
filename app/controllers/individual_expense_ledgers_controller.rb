59957191fc63e0cbe64e6c17b50b6b87ca4ceb93
Raghu Betina <raghu@firstdraft.com>
2019-12-05 03:09:01 +0000

    Only individual_expense_ledger user can access show page

--- app/controllers/individual_expense_ledgers_controller.rb
+++ app/controllers/individual_expense_ledgers_controller.rb
@@ -1,5 +1,5 @@
 class IndividualExpenseLedgersController < ApplicationController
-  before_action :current_user_must_be_individual_expense_ledger_user, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_individual_expense_ledger_user, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_individual_expense_ledger_user
     individual_expense_ledger = IndividualExpenseLedger.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
