e4fb3029df119ea4084ef05bdb1fd0890f193d83
Raghu Betina <raghu@firstdraft.com>
2019-12-05 03:08:44 +0000

    Whitelist columns in Group for ActiveAdmin

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Group do
+
+ permit_params :group_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
