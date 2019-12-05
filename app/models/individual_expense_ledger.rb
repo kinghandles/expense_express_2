bc9f879b067544cb46272b90d1f771758332c22c
Raghu Betina <raghu@firstdraft.com>
2019-12-05 03:08:16 +0000

    Validate presence of individual_expense_ledgers.expense_id

--- app/models/individual_expense_ledger.rb
+++ app/models/individual_expense_ledger.rb
@@ -5,6 +5,8 @@ class IndividualExpenseLedger < ApplicationRecord
 
   # Validations
 
+  validates :expense_id, :presence => true
+
   validates :user_id, :uniqueness => true
 
   validates :user_id, :presence => true
